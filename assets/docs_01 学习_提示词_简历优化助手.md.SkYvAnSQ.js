f34805a0b4294f7d407ce6769445ac33127a5b81
muchuang1024 <muchuang1024@users.noreply.github.com>
2024-05-02 09:38:05 +0000

    deploy: 2d5d5b51a438ae14b5fa000f0b3213b09754385a

--- assets/docs_01 学习_提示词_简历优化助手.md.SkYvAnSQ.js
+++ assets/docs_01 学习_提示词_简历优化助手.md.SkYvAnSQ.js
@@ -27,6 +27,34 @@ import{_ as s,c as a,o as n,a4 as i}from"./chunks/framework.4aTu-Nia.js";const y
 <span class="line"><span>深入理解 Redis 线程模型，熟练掌握核心数据结构和使用场景。深入了其解执行流程、持久化和过期策略，熟练解决缓存穿透、缓存及穿、缓存雪崩等问题</span></span>
 <span class="line"><span></span></span>
 <span class="line"><span>有线上问题排查经验</span></span>
+<span class="line"><span>---</span></span></code></pre></div><h2 id="根据-jd-优化项目经验" tabindex="-1">根据 JD 优化项目经验 <a class="header-anchor" href="#根据-jd-优化项目经验" aria-label="Permalink to &quot;根据 JD 优化项目经验&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>请基于下面这个 JD 和项目描述，优化 JD 对应的项目描述：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>项目描述：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>---</span></span>
+<span class="line"><span>项目名称：资金池系统</span></span>
+<span class="line"><span>项目描述：微信支付宝等渠道流水自动匹配银行到账流水的SASS系统，只需导入excel，即可解决财务人员对账、项目人员认账的问题</span></span>
+<span class="line"><span>项目职责：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>- 作为项目负责人，完成了整体的项目架构、数据库设计、产品交付、Code Review</span></span>
+<span class="line"><span>- 基于Laravel + Redis实现任务队列，异步处理对账任务，实现并发处理</span></span>
+<span class="line"><span>- 通过业务流程和SQL优化，解决出现的死锁问题</span></span>
+<span class="line"><span>- 通过使用原生SQL批量写入代替ORM插入数据，大幅度提升数据插入性能</span></span>
+<span class="line"><span>---</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>JD：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>---</span></span>
+<span class="line"><span>1、负责公司C端核心产品及服务的后端架构和业务功能的研究、设计、开发 </span></span>
+<span class="line"><span>2、根据业务需求，进行技术设计和技术选型，制定合理的技术方案，并落地实现保障C端服务的性能、可扩展性、可用性 </span></span>
+<span class="line"><span>3、参与AI大模型类产品功能的研发，持续提升服务交付效率和质量</span></span>
+<span class="line"><span>任职资格</span></span>
+<span class="line"><span>任职资格 </span></span>
+<span class="line"><span>1、统招本科及以上学历，计算机、软件等相关专业优先 </span></span>
+<span class="line"><span>2、3年以上开发经验，熟练使用Go/PHP语言，熟悉Go/PHP常用框架，精通Mysql, Redis, Kafka等常用工具，有一定的接口调优能力和一定的算法设计能力</span></span>
+<span class="line"><span>3、学习能力强，有较好的沟通能力，能迅速融入团队；较强的逻辑思维能力，具有较强产品意识 </span></span>
+<span class="line"><span>4、热爱互联网，团队合作意识强，心态好，有责任心 </span></span>
+<span class="line"><span>5、有AI大模型对接经验者优先</span></span>
 <span class="line"><span>---</span></span></code></pre></div><h2 id="优化专业技能" tabindex="-1">优化专业技能 <a class="header-anchor" href="#优化专业技能" aria-label="Permalink to &quot;优化专业技能&quot;">​</a></h2><div class="language-SQL vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">SQL</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">请基于下面这个专业技能描述模板，优化6年软件测试的专业技能描述：</span></span>
 <span class="line"></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">需优化的专业技能描述：</span></span>
@@ -49,7 +77,7 @@ import{_ as s,c as a,o as n,a4 as i}from"./chunks/framework.4aTu-Nia.js";const y
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">深入理解 Redis 线程模型，熟练掌握核心数据结构和使用场景。深入了其解执行流程、持久化和过期策略，熟练解决缓存穿透、缓存及穿、缓存雪崩等问题</span></span>
 <span class="line"></span>
 <span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">有线上问题排查经验</span></span>
-<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">---</span></span></code></pre></div><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span></span></span></code></pre></div><h2 id="优化项目经验" tabindex="-1">优化项目经验 <a class="header-anchor" href="#优化项目经验" aria-label="Permalink to &quot;优化项目经验&quot;">​</a></h2><div class="language-SQL vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">SQL</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">请优化这段项目描述，重点突出6年软件测试应该有的项目经验，有量化的指标，具体使用xxx技术解决什么问题</span></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;">---</span></span></code></pre></div><h2 id="优化项目经验" tabindex="-1">优化项目经验 <a class="header-anchor" href="#优化项目经验" aria-label="Permalink to &quot;优化项目经验&quot;">​</a></h2><div class="language-SQL vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">SQL</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">请优化这段项目描述，重点突出6年软件测试应该有的项目经验，有量化的指标，具体使用xxx技术解决什么问题</span></span>
 <span class="line"></span>
 <span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">项目一 </span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">: 一码 App</span></span>
 <span class="line"></span>
@@ -71,4 +99,4 @@ import{_ as s,c as a,o as n,a4 as i}from"./chunks/framework.4aTu-Nia.js";const y
 <span class="line"></span>
 <span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">2</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">、</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">安全测试</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">：通过安全测试，识别并修复了安全风险和漏洞，提高了App的安全性。</span></span>
 <span class="line"></span>
-<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">3</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">、</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">用户体验测试</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">：对界面布局、操作流程和提示信息进行了测试，改善了用户体验，提高了用户满意度</span></span></code></pre></div>`,7),h=[l];function e(t,k,E,d,c,r){return n(),a("div",null,h)}const o=s(p,[["render",e]]);export{y as __pageData,o as default};
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">3</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">、</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">用户体验测试</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">：对界面布局、操作流程和提示信息进行了测试，改善了用户体验，提高了用户满意度</span></span></code></pre></div>`,8),e=[l];function h(t,k,c,E,d,r){return n(),a("div",null,e)}const o=s(p,[["render",h]]);export{y as __pageData,o as default};
